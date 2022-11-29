470e0b769b5b3a5f8a3e908cc9b368f53c92d529
idamakela <idamakela98@gmail.com>
2022-11-29 16:34:00 +0100

    test if answer contains user input kindof done

--- js/game.js
+++ js/game.js
@@ -1,48 +1,88 @@
-let words = ["stake", "suite", "agile", "guest", "angel", "grave", "spare", "twist", "score", "drown"];
+let words = [
+    "stake", 
+    "suite", 
+    "agile", 
+    "guest", 
+    "angel", 
+    "grave", 
+    "spare", 
+    "twist", 
+    "score", 
+    "drown"
+];
 
+let answer = "";
+let answerLetters = [];
+let userInput = "";
+let maxWrong = 7;
+let mistakes = 0;
+const TEST_PATTERN = /^[a-z]{1}$/i;
 
-//start onclick function
 document.getElementById("startButton").onclick = function() {
-    start();
-}
-
-//loop for game keep on running
-/*while(userStatus = false) {
-    return gameLoop();
-}*/
-
-//start game function
-function start() {
-    alert("You have started the game") + gameLoop();
-}
-
-
-//game loop while false
-function gameLoop() {
-    let startGame = prompt("Enter a letter")
-}
-
-
-
-//random word function
-function randomWord() {
-    let gameArray = words;
-    let selectedWord = gameArray[Math.floor(Math.random() * gameArray.length)]
-
-    return selectedWord;
-}
-
-
-//iterate thourgh an array
-function iterateArray(gameArray) {
-    if(Array.isArray(gameArray)) {
-        gameArray.forEach(function(value) {
-            console.log(value);
-        })
+    randomWord();
+    console.log(answer);
+    userInput = prompt("You have started the game!\n\nEnter a letter.");
+    
+    if(userInput.match(TEST_PATTERN)) {
+        testGuess(answer);
+    } else {
+        alert("try again");
     }
 }
 
-//userinput function
+function randomWord() {
+    answer = words[Math.floor(Math.random() * words.length)];
+}
+
+function doesItContain() {
+    if(answer.toLocaleLowerCase().includes(userInput)) {
+        alert("it does contain " + userInput);
+    } else {
+        alert("nope")
+    }
+}
 
 
-//test function
+function testGuess() {
+    if(Array.isArray(answerLetters)) {
+        for(i = 0; i < maxWrong; i++) { 
+            answerLetters.forEach(doesItContain());
+        }
+    } else {
+        alert("broken game");
+    }
+}
+
+
+
+
+
+
+//where all the functions are connected???
+function gameLoop() {
+    let randomWord = randomWord();
+    let userInput = prompt("Enter a letter");
+
+    while (false) {
+
+    }
+
+
+}
+
+
+//testing user input 
+function userInputTest() {
+    let userInput = prompt("Enter only one letter");
+    let testPattern = /^[a-z]{1}$/i;
+
+    let randomWord = randomWord();
+
+    if(userInput.match(testPattern)) {
+        //game loop for testing input to randomWord 
+        
+    } else {
+        alert("Invalid input! Try again.");
+        //run loop again
+    }
+}
