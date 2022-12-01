da327cbcf7447b0a33d608761a05e34ca2f7a50f
idamakela <idamakela98@gmail.com>
2022-12-01 18:19:38 +0100

    make minor changes

--- js/game.js
+++ js/game.js
@@ -1,49 +1,61 @@
+//game elements
+/*
+1. DONE array of words
+2. DONE random word "function"
+3. DONE emty array for the generated word (aka the answer for the game)
+4. DONE loop for word length, to show the user the guessed letters and their index place
+5. DONE empty array for user guessed letters
+6. DONE variable + logic for how many tries the user has left
+7. DONE variable + logic for remaining letters 
+8. start the game
+9. while loop for when there still are guesses left.
+    a. display tries left
+    b. prompt for user guess input
+        i. test weather it has already been inputted
+    c. test weather user only inputs one letter
+    d. test weather answer does or does not inclued user guess
+
+*/
+
+
 let words = [
-    "stake", 
-    "suite", 
-    "agile", 
-    "guest", 
-    "angel", 
-    "grave", 
-    "spare", 
-    "twist", 
-    "score", 
+    "stake",
+    "suite",
+    "agile",
+    "guest",
+    "angel",
+    "grave",
+    "spare",
+    "twist",
+    "score",
     "drown"
 ];
 
 let answer = "";
 let answerLetters = [];
 let userInput = "";
-let maxWrong = 7;
-let mistakes = 0;
+let userGuessed = [];
+let remainingLetters = answer.length;
+let maxWrong = answer.length + 2;
 const TEST_PATTERN = /^[a-z]{1}$/i;
 
-document.getElementById("startButton").onclick = function() {
+let startGame = document.getElementById("startButton").addEventListener("click", gameFunction);
+
+function gameFunction() {
     randomWord();
-    console.log(answer);
     displayGuesses();
 
-    userInput = prompt("You have started the game!\n\nEnter a letter.\n" 
-    + answerLetters.join(" ") + "\n"
-    );
-    
-    if(userInput.match(TEST_PATTERN)) {
+    userInput = prompt("\nYou have started the game!\n\nEnter a letter.\n" + answerLetters.join(" ") + "\n");
+
+    if (userInput.match(TEST_PATTERN)) {
         testGuess(answer);
     } else {
-        alert("try again");
+        alert("\nEnter only one letter");
     }
 }
 
 function randomWord() {
     answer = words[Math.floor(Math.random() * words.length)];
-}
-
-function doesItContain() {
-    if(answer.toLocaleLowerCase().includes(userInput)) {
-        alert("it does contain " + userInput);
-    } else {
-        alert("nope")
-    }
 }
 
 function displayGuesses() {
@@ -52,50 +64,26 @@ function displayGuesses() {
     }
 }
 
-
-
 function testGuess() {
-    if(Array.isArray(answerLetters)) {
-        for(i = 0; i < maxWrong; i++) { 
-            answerLetters.forEach(doesItContain());
-        }
+    if (Array.isArray(answerLetters)) {
+        answerLetters.forEach(doesItContain);
     } else {
-        alert("broken game");
+        alert("Broken game");
     }
 }
 
-
-
-
-
-
-//where all the functions are connected???
-function gameLoop() {
-    /*let randomWord = randomWord();
-    let userInput = prompt("Enter a letter");
-
-    while (false) {
-
-    }*/
-
-
-
-
+function doesItContain() {
+    if (answer.toLocaleLowerCase().includes(userInput)) {
+        alert("it does contain " + userInput);
+    } else {
+        alert("nope")
+    }
 }
 
-
-//testing user input 
-function userInputTest() {
-    let userInput = prompt("Enter only one letter");
-    let testPattern = /^[a-z]{1}$/i;
-
-    let randomWord = randomWord();
-
-    if(userInput.match(testPattern)) {
-        //game loop for testing input to randomWord 
-        
+function guessesLeft() {
+    if (userGuessed >= maxWrong) {
+        alert("You have lost the game")
     } else {
-        alert("Invalid input! Try again.");
-        //run loop again
+        
     }
 }
